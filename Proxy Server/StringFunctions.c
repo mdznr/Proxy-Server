b15030e810150bd49fd2bf3c0183e94d78cd4e72
Matt Zanchelli <matt@mdznr.com>
2014-03-02 14:24:48 -0500

    Added string functions for working with substrings.

--- Proxy Server/StringFunctions.c
+++ Proxy Server/StringFunctions.c
@@ -7,10 +7,72 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "Boolean.h"
 
 bool stringEquality(const char *s1, const char *s2)
 {
 	return strcmp(s1, s2) == 0;
+}
+
+int indexOfFirstOccuranceOfCharacterInString(const char *string, const char character)
+{
+	// Must not be NULL.
+	if ( string == NULL ) {
+		return -1;
+	}
+	
+	// The index of the character.
+	int index = 0;
+	while ( string[index] != '\0' ) {
+		if ( string[index] == character ) {
+			return index;
+		}
+		++index;
+	}
+	
+	// Could not find the character in the string.
+	return -1;
+}
+
+char *substr(const char *begin, const char *end)
+{
+	// End must be after begin.
+	if ( end <= begin ) {
+		return NULL;
+	}
+	
+	// The length of the string is the difference in pointers
+	size_t stringLength = (end-begin);
+	
+	// Allocate size of the string length plus one for the null-terminator.
+	char *substring = malloc(sizeof(char) * (stringLength + 1) );
+	
+	// Avoid diqualifying const.
+	char *beginning = (char *) begin;
+	
+	// Copy over substring.
+	memcpy(substring, beginning, sizeof(char) * stringLength);
+	
+	// Terminate the substring.
+	substring[stringLength] = '\0';
+	
+	return substring;
+}
+
+char *substringUntilCharacter(const char *string, const char character)
+{
+	char *occurance = strchr(string, (int) character);
+	char *substring = substr(string, occurance);
+	
+	return substring;
+}
+
+char *prefixOfStringUpUntilString(const char *string, const char *substring)
+{
+	char *occurance = strstr(string, substring);
+	char *prefix = substr(string, occurance);
+	
+	return prefix;
 }
--- Proxy Server/StringFunctions.h
+++ Proxy Server/StringFunctions.h
@@ -17,4 +17,31 @@
 /// @return Whether or not the strings are equal.
 bool stringEquality(const char *s1, const char *s2);
 
+/// Find the index of the first occurance of a character in a string.
+/// @param string The null-terminated byte stream.
+/// @param character The character to search for.
+/// @return The index at which the first instace of @c character exists in @c string.
+/// @discussion Returns -1 if not found.
+int indexOfFirstOccuranceOfCharacterInString(const char *string, const char character);
+
+/// Create a substring.
+/// @param begin The pointer to the beginning of the string.
+/// @param end The pointer to the end of the substring.
+/// @return A newly allocated string for the substring.
+char *substr(const char *begin, const char *end);
+
+/// Get the prefix of a string until a specific character.
+/// @param string The null-terminated byte stream.
+/// @param character The character to search for.
+/// @return A newly allocated string containing the prefix of @c string up until the first occurance of @c character.
+/// @discussion Returns @c NULL if @c character does not exist in @c string.
+char *prefixUntilCharacter(const char *string, const char character);
+
+/// Get the prefix of a string until a substring.
+/// @param string The null-terminated byte stream.
+/// @param substring The substring to end at.
+/// @return A newly allocated string containing the prefix of @c string up until the first cocurance of @c substring.
+/// @discussion Returns @c NULL if @c substring does not exist in @c string.
+char *prefixOfStringUpUntilString(const char *string, const char *substring);
+
 #endif
