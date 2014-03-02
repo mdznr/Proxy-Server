97c99129bf11cdf6844718266ee2629dc438dd04
Matt Zanchelli <matt@mdznr.com>
2014-03-01 21:34:57 -0500

    New HTTP.h header file for including both HTTPRequest and HTTPResponse

---
+++ Proxy Server/HTTP.h
@@ -0,0 +1,15 @@
+//
+//  HTTP.h
+//  Proxy Server
+//
+//  Created by Matt Zanchelli on 3/1/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#ifndef Proxy_Server_HTTP_h
+#define Proxy_Server_HTTP_h
+
+#include "HTTPRequest.h"
+#include "HTTPResponse.h"
+
+#endif
