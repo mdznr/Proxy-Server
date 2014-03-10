7664433e04ded2c72a93ab7fb21c65cde993e057
Matt Zanchelli <matt@mdznr.com>
2014-03-10 02:03:41 -0400

    No longer need print statements to debug atomic increment of statistics.

--- Proxy Server/Statistics.c
+++ Proxy Server/Statistics.c
@@ -14,21 +14,15 @@
 
 void incrementNumberOfSuccessfulRequests()
 {
-	printf("Incrementing numberOfSuccessfulRequests: %u\n", *numberOfSuccessfulRequests);
 	SYNCHRONOUS_BLOCK((*numberOfSuccessfulRequests)++);
-	printf("Incremented numberOfSuccessfulRequests: %u\n", *numberOfSuccessfulRequests);
 }
 
 void incrementNumberOfFilteredRequests()
 {
-	printf("Incrementing numberOfFilteredRequests: %u\n", *numberOfFilteredRequests);
 	SYNCHRONOUS_BLOCK((*numberOfFilteredRequests)++);
-	printf("Incremented numberOfFilteredRequests: %u\n", *numberOfFilteredRequests);
 }
 
 void incrementNumberOfErroredRequests()
 {
-	printf("Incrementing numberOfErroredRequests: %u\n", *numberOfErroredRequests);
 	SYNCHRONOUS_BLOCK((*numberOfErroredRequests)++);
-	printf("Incremented numberOfErroredRequests: %u\n", *numberOfErroredRequests);
 }
