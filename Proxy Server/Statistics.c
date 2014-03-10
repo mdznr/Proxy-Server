8c9f9f41d200065833bb83cfb821030c276086e9
Matt Zanchelli <matt@mdznr.com>
2014-03-10 01:33:51 -0400

    Printing out statistical incrementation debugging information.

--- Proxy Server/Statistics.c
+++ Proxy Server/Statistics.c
@@ -14,15 +14,21 @@
 
 void incrementNumberOfSuccessfulRequests()
 {
+	printf("Incrementing numberOfSuccessfulRequests: %u", numberOfSuccessfulRequests);
 	SYNCHRONOUS_BLOCK(numberOfSuccessfulRequests++);
+	printf("Incremented numberOfSuccessfulRequests: %u", numberOfSuccessfulRequests);
 }
 
 void incrementNumberOfFilteredRequests()
 {
+	printf("Incrementing numberOfFilteredRequests: %u", numberOfSuccessfulRequests);
 	SYNCHRONOUS_BLOCK(numberOfFilteredRequests++);
+	printf("Incremented numberOfFilteredRequests: %u", numberOfSuccessfulRequests);
 }
 
 void incrementNumberOfErroredRequests()
 {
+	printf("Incrementing numberOfErroredRequests: %u", numberOfSuccessfulRequests);
 	SYNCHRONOUS_BLOCK(numberOfErroredRequests++);
+	printf("Incremented numberOfErroredRequests: %u", numberOfSuccessfulRequests);
 }
